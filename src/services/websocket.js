6c103106cd7e267c521131c3d589c7451cd7208a
Pavan9567 <pavank9567@gmail.com>
2025-02-15 12:05:15 +0530

    Chat-Api Frontend

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,4 +1,4 @@
-const WS_URL = "ws://localhost:1337";
+const WS_URL = "https://chat-api-backend-m7ft.onrender.com";
 const ws = new WebSocket(WS_URL);
 
 ws.onopen = () => console.log("Connected to WebSocket server");
